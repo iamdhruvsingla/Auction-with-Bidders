4ecd4d2784d3bfdc9291d25c30e9208e2d6d0000
Dhruv Singla <136337908+iamdhruvsingla@users.noreply.github.com>
2024-06-25 17:58:01 +0530

    Update auction.py

--- auction.py
+++ auction.py
@@ -6,30 +6,6 @@ def clear():
     if os.name != "nt":
         os.system('clear')
 
-# FIRST TRY
-# names_and_bids = {}
-
-# # def bid():
-# #     name = input("Enter your name\n")
-# #     bid_price = input("Enter your bid price\n")
-# #     names_and_bids[name]= bid_price
-# #     more_players = input("Is there anyone else who wants to bid? yes or no\n").lower()
-# #     if more_players == "yes":
-# #         bid()
-# #     else:
-# #         for names in names_and_bids:
-# #             bids = names_and_bids[names]
-# #             bids_array = []
-# #             bids_array.append(bids)
-# #             highest = str(0)
-# #             for i in range(0, len(bids_array)):
-# #                 if bids_array[i] > highest:
-# #                     highest = bids_array[i]
-# #     print(f"Highest bid is {highest}")
-# # bid()
-
-
-# SECOND TRY
 bids = {}
 bidding_finished = False
 
